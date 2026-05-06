b8060fed1727062384397abfea35e5d4b6e53d18
DqHuY07 <dinhquochuybh@gmail.com>
2026-05-06 22:15:54 +0700

    feat: first comment

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+  @Test
+  public void testOne() {
+    assertEquals(1,1);
+  }
+
+  @Test
+  public void TestTwo() {
+    assertEquals(2,2);
+  }
+}
