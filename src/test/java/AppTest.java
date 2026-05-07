23a34ee3cec2fb8cc1ce9f02ca78cacbbce0d2ec
DqHuY07 <dinhquochuybh@gmail.com>
2026-05-07 11:10:06 +0700

    feat: commit 1, add() test pass

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,24 +7,4 @@ public class AppTest {
   public void testOne() {
     assertEquals(5, App.add(2,3));
   }
-
-  @Test
-  public void TestTwo() {
-    assertEquals(-5,App.add(-2,-3));
-  }
-
-  @Test
-  public void testThree() {
-    assertEquals(-1,App.add(2,-3));
-  }
-
-  @Test
-  public void testFour() {
-    assertEquals(3,App.add(3,0));
-  }
-
-  @Test
-  public void testFive() {
-    assertEquals(Integer.MAX_VALUE, App.add(Integer.MAX_VALUE, 0));
-  }
 }
