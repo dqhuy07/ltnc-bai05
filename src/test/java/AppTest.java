05a8b9c5ff5bcb9b15bfdd47f7b5f52814f9d946
DqHuY07 <dinhquochuybh@gmail.com>
2026-05-07 11:13:52 +0700

    feat: commit 1, add() test pass

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,17 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AppTest {
   @Test
   public void testOne() {
     assertEquals(5, App.add(2,3));
+  }
+
+  @Test
+  public void testTwo() {
+    App app = new App();
+    assertNotNull(app);
   }
 }
